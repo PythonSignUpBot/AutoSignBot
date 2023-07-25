4a201958e2d4d1d98ecfc82bdee8a09c8bed81cb
BabyDog1998 <dreamglory04@gmail.com>
2023-07-25 21:09:27 +0300

    Updated.

--- signbot.py
+++ signbot.py
@@ -16,5 +16,7 @@ for row in range(0, dataframe1.max_row):
                 month = random.randint(1, 12)
                 year = random.randint(1958, 1994)
                 birthday = str(day) + '/' + str(month) + '/' + str(year)
-                dataframe1.cell(row, 2).value = birthday
+                dataframe1.cell(row, 2).value = 3
                 print(col[row].value)
+
+dataframe.save("signupinfo.xlsx")
